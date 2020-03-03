0a2982e16d1d741e0b6944b45a3e52f3d3a16fa2
Vetle Leinonen-Roeim <vetle@roeim.net>
2020-03-03 21:03:49 +0100

    Initiializing project

---
+++ src/main/kotlin/net/roeim/test/test/TestApplication.kt
@@ -0,0 +1,11 @@
+package net.roeim.test.test
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TestApplication
+
+fun main(args: Array<String>) {
+	runApplication<TestApplication>(*args)
+}
---
+++ src/test/kotlin/net/roeim/test/test/TestApplicationTests.kt
@@ -0,0 +1,13 @@
+package net.roeim.test.test
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TestApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
