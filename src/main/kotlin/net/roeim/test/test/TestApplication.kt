ef622293e4ad6c47bd643f9a4b0a9bf49e5ebeee
Vetle Leinonen-Roeim <vetle@roeim.net>
2020-03-03 21:32:00 +0100

    Hello world

--- src/main/kotlin/net/roeim/test/test/TestApplication.kt
+++ src/main/kotlin/net/roeim/test/test/TestApplication.kt
@@ -1,10 +1,15 @@
 package net.roeim.test.test
 
+import org.springframework.boot.CommandLineRunner
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class TestApplication
+class TestApplication : CommandLineRunner {
+	override fun run(vararg args: String?) {
+		println("Hello world!")
+	}
+}
 
 fun main(args: Array<String>) {
 	runApplication<TestApplication>(*args)
