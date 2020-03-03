39ffae7c49cf757b670d148f346ae5814e0d2989
Vetle Leinonen-Roeim <vetle@roeim.net>
2020-03-03 22:52:24 +0100

    Starting on REST API

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,9 +3,9 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
     id("org.springframework.boot") version "2.2.5.RELEASE"
     id("io.spring.dependency-management") version "1.0.9.RELEASE"
-	id("org.sonarqube") version "2.7"
-	kotlin("plugin.spring") version "1.3.61"
-	kotlin("jvm") version "1.3.61"
+    id("org.sonarqube") version "2.7"
+    kotlin("jvm") version "1.3.61"
+    kotlin("plugin.spring") version "1.3.61"
 }
 
 group = "net.roeim.test"
@@ -17,7 +17,8 @@ repositories {
 }
 
 dependencies {
-    implementation("org.springframework.boot:spring-boot-starter")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
     testImplementation("org.springframework.boot:spring-boot-starter-test") {
